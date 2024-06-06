444eedbdba1afd63b48d4348c34c03d2b472b659
Jim Winkelmann <jim@presagetech.com>
2024-06-06 10:38:34 -0500

    fix: upload speed and camera

--- Test SmartSpectra SDK/ContentView.swift
+++ Test SmartSpectra SDK/ContentView.swift
@@ -15,31 +15,24 @@ struct ContentView: View {
             // Text("Strict Pulse Rate Value: \(String(format: "%.2f", sdk.strictPulseRate))")
             // Text("Stric Breathing Rate Value: \(String(format: "%.2f", sdk.strictBreathingRate))")
             
-            // Example of extracting, sorting and plotting the Pulse Pleth Waveform from sdk.jsonMetrics
-            if let json = sdk.jsonMetrics {
-                let pulse = json["pulse"] as? [String: [String: Any]]
-                if let pulsePleth = pulse?["hr_trace"] as? [String: [String: Double]] {
-                    // The data might not be in increasing time order so sort it on time
-                    let sortedData = sortTimeValuePairs(from: pulsePleth)
-                    // plot the data
-                    LineChartView(times: sortedData.sortedTimes, values: sortedData.sortedValues).frame(height: 200)
-                }
-            }else {
+            // Example of extracting, sorting and plotting the Pulse Pleth Waveform from sdk.
+            if !SmartSpectraIosSDK.shared.pulsePleth.isEmpty {
+                LineChartView(
+                    orderedPairs: SmartSpectraIosSDK.shared.pulsePleth
+                ).frame(height: 200)
+            } else {
                 // No Data avialable so display a flat line
                 createFlatLine()
             }
             
-            // Example of extracting, sorting and plotting the Pulse Pleth Waveform from sdk.jsonMetrics
-            if let json = sdk.jsonMetrics {
-                let breath = json["breath"] as? [String: [String: Any]]
-                if let breathingPleth = breath?["rr_trace"] as? [String: [String: Double]] {
-                    // The data might not be in increasing time order so sort it on time
-                    let sortedData = sortTimeValuePairs(from: breathingPleth)
-                    // plot the data
-                    LineChartView(times: sortedData.sortedTimes, values: sortedData.sortedValues).frame(height: 200)
-                }
-            }else {
-                // No Data avialable so display a flat line
+            // Example of extracting, sorting and plotting the Pulse Pleth Waveform from sdk.
+            // plot the data
+            if !SmartSpectraIosSDK.shared.breathingPleth.isEmpty {
+                LineChartView(
+                    orderedPairs: SmartSpectraIosSDK.shared.breathingPleth
+                ).frame(height: 200)
+            } else {
+                // No Data available so display a flat line
                 createFlatLine()
             }
         }
@@ -52,53 +45,25 @@ struct ContentView: View {
 
 // Below are some helper functions to extract and visualize data from SmartSpectra
 
-/// Sorts time and value pairs extracted from SmartSpectra sdk.jsonMetrics
-///
-/// The function takes a dictionary where keys represent time as strings and values
-/// are dictionaries with a "value" key holding a Double. It extracts these pairs,
-/// filters out any invalid entries where the time cannot be converted to Double or
-/// the value is missing, sorts them by time, and then returns them in separate arrays.
-///
-/// - Parameter jsonMetric: A dictionary structured as [String: [String: Double]] where
-///   the outer string key is the time and the inner dictionary contains a "value" key with a Double.
-/// - Returns: A tuple containing two arrays: `sortedTimes` with all valid times as Double,
-///   and `sortedValues` with corresponding Double values, both arrays are sorted by time.
-func sortTimeValuePairs(from jsonMetric: [String: [String: Double]]) -> (sortedTimes: [Double], sortedValues: [Double]) {
-    // Convert dictionary into array of tuples, filtering out invalid data
-    let timeHrPairs = jsonMetric.compactMap { key, value -> (time: Double, value: Double)? in
-        if let time = Double(key), let value = value["value"] {
-            return (time, value)
-        }
-        return nil  // Skip invalid entries rather than substituting with (0.0, 0.0)
-    }.sorted(by: { $0.time < $1.time })  // Sort by time
-
-    // Extract sorted times and HR values
-    let sortedTimes = timeHrPairs.map { $0.time }
-    let sortedValues = timeHrPairs.map { $0.value }
-
-    return (sortedTimes, sortedValues)
-}
-
 /// `LineChartView` renders a line chart based on provided time and value data.
 /// This View normalizes time and value data points to fit within the view's dimensions,
 /// ensuring that the line chart accurately represents relative differences in data across its range.
 struct LineChartView: View {
-    let times: [Double]
-    let values: [Double]
+    let orderedPairs: [(time: Double, value: Double)]
     
-    private var normalizedData: [(x: Double, y: Double)] {
-        let maxTime = times.max() ?? 1
-        let maxValue = values.max() ?? 1
-        let minValue = values.min() ?? 0
-        let valueRange = maxValue - minValue // The range of your HR values
+    private var normalizedData: [(x: Double, y: Double)] {        
+        guard let maxTime = orderedPairs.max(by: {$0.time < $1.time})?.time else { return [] }
+        guard let maxValue = orderedPairs.max(by: {$0.value < $1.value}) else { return [] }
+        guard let minValue = orderedPairs.min(by: {$0.value < $1.value}) else { return [] }
+        let valueRange = maxValue.value - minValue.value // The range of your HR values
 
-        return zip(times, values).map { time, value in
-            let normalizedX = Double(time) / maxTime
+        return orderedPairs.map { orderedPair in
+            let normalizedX = Double(orderedPair.time) / maxTime
             let normalizedY: Double
             if valueRange == 0 { // Check if valueRange is zero
                 normalizedY = 0.5 // Center the line if all values are the same
             } else {
-                normalizedY = (value - minValue) / valueRange // Normalize y-value to be within 0...1, adjusting for minValue and valueRange
+                normalizedY = (orderedPair.value - minValue.value) / valueRange // Normalize y-value to be within 0...1, adjusting for minValue and valueRange
             }
             return (x: normalizedX, y: normalizedY)
         }
@@ -128,8 +93,7 @@ struct LineChartView: View {
 
 func createFlatLine() -> some View {
     LineChartView(
-        times: (1...10).map { Double($0) }, // Generates an array of Double values from 1 to 10
-        values: Array(repeating: 0.0, count: 10) // Generates an array with ten 0.0 values
+        orderedPairs: (1...10).map { (0.0, Double($0)) }
     )
     .frame(height: 200) // Sets the height of the LineChartView
 }
